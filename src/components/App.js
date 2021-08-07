8ea093bb5baf3772907b7d9ea80127efa9ab9e4b
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-07 15:32:53 +0200

    render timer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+// React
+import React, { useState } from 'react';
+// Components
+import Timer from './Timer';
+// Styles
 import '../stylesheets/App.scss';
 
 const App = (props) => {
   return (
     <>
-      <div>Start</div>
+      <Timer />
     </>
   );
 };
