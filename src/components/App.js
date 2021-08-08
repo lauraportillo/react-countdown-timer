a3b689eeeaba9138a1f36afc7b5493d5611fc606
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-08 16:07:12 +0200

    countdown working

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,74 @@
-// React
-import React, { useState, useEffect } from 'react';
-// Components
-import Timer from './Timer';
-// Styles
-import '../stylesheets/App.scss';
+// // React
+// import React, { useState, useEffect } from 'react';
+// // Components
+// import Timer from './Timer';
+// // Styles
+// import '../stylesheets/App.scss';
 
-const App = () => {
-  //state
-  const [minute, setMinute] = useState('60');
-  const [second, setSecond] = useState('00');
-  const [isActive, setIsActive] = useState(false);
-  const [counter, setCounter] = useState(0);
+// const App = () => {
+//   //state
+//   const [minute, setMinute] = useState('20');
+//   const [second, setSecond] = useState('40');
+//   const [isActive, setIsActive] = useState(false);
+//   const [counter, setCounter] = useState(0);
 
-  //useEffect
-  useEffect(() => {
-    let intervalId;
+//   //useEffect
+//   useEffect(() => {
+//     let intervalId;
 
-    if (isActive) {
-      intervalId = setInterval(() => {
-        const secondCounter = counter % 60;
-        const minuteCounter = Math.floor(counter / 60);
+//     if (isActive) {
+//       intervalId = setInterval(() => {
+//         const secondCounter = counter % 60;
+//         const minuteCounter = Math.floor(counter / 60);
 
-        const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}` : secondCounter;
-        const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}` : minuteCounter;
+//         const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}` : secondCounter;
+//         const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}` : minuteCounter;
 
-        setSecond(computedSecond);
-        setMinute(computedMinute);
+//         setSecond(computedSecond);
+//         setMinute(computedMinute);
 
-        setCounter((counter) => counter + 1);
-      }, 1000);
-    }
+//         setCounter((counter) => counter + 1);
+//       }, 1000);
+//     }
 
-    return () => clearInterval(intervalId);
-  }, [isActive, counter]);
+//     return () => clearInterval(intervalId);
+//   }, [isActive, counter]);
 
-  const handleReset = () => {
-    setIsActive(false);
-    setCounter(0);
-    setSecond('00');
-    setMinute('00');
-  };
-
-  return (
-    <>
-      <Timer
-        second={second}
-        minute={minute}
-        isActive={isActive}
-        counter={counter}
-        setIsActive={setIsActive}
-        handleReset={handleReset}
-      />
-    </>
-  );
-};
-
-export default App;
-
-// import './App.css';
-// import CountDownTimer from './CountDownTimer';
-
-// function App() {
-
-//   const hoursMinSecs = {hours:1, minutes: 20, seconds: 40}
+//   const handleReset = () => {
+//     setIsActive(false);
+//     setCounter(0);
+//     setSecond('00');
+//     setMinute('00');
+//   };
 
 //   return (
-//     <div className="App">
-//         <CountDownTimer hoursMinSecs={hoursMinSecs}/>
-//     </div>
+//     <>
+//       <Timer
+//         second={second}
+//         minute={minute}
+//         isActive={isActive}
+//         counter={counter}
+//         setIsActive={setIsActive}
+//         handleReset={handleReset}
+//       />
+//     </>
 //   );
-// }
+// };
 
 // export default App;
+
+// COPIA 1
+import Timer from './Timer';
+import '../stylesheets/App.scss';
+
+function App() {
+  const hoursMinSecs = { hours: 1, minutes: 20, seconds: 40 };
+
+  return (
+    <div className="App">
+      <Timer hoursMinSecs={hoursMinSecs} />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,65 +1,65 @@
-import '../stylesheets/Timer.scss';
+// // Styles
+// import '../stylesheets/Timer.scss';
 
-const Timer = (props) => {
-  const handleReset = () => {
-    props.handleReset();
+// const Timer = (props) => {
+//   const handleReset = () => {
+//     props.handleReset();
+//   };
+
+//   return (
+//     <div>
+//       <div className="counter">
+//         <span className="counter__min">{props.minute}</span>
+//         <span>:</span>
+//         <span className="counter__sec">{props.second}</span>
+//       </div>
+//       <div className="btns">
+//         <button onClick={() => props.setIsActive(!props.isActive)} className="btns__start">
+//           {props.isActive ? 'Pause' : 'Start'}
+//         </button>
+//         <button onClick={handleReset} className="btns__reset">
+//           Reset
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Timer;
+
+// COPIA 1
+
+import React from 'react';
+
+const Timer = ({ hoursMinSecs }) => {
+  const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
+  const [[hrs, mins, secs], setTime] = React.useState([hours, minutes, seconds]);
+
+  const tick = () => {
+    if (hrs === 0 && mins === 0 && secs === 0) reset();
+    else if (mins === 0 && secs === 0) {
+      setTime([hrs - 1, 59, 59]);
+    } else if (secs === 0) {
+      setTime([hrs, mins - 1, 59]);
+    } else {
+      setTime([hrs, mins, secs - 1]);
+    }
   };
+
+  const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+  React.useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
 
   return (
     <div>
-      <div className="counter">
-        <span className="counter__min">{props.minute}</span>
-        <span>:</span>
-        <span className="counter__sec">{props.second}</span>
-      </div>
-      <div className="btns">
-        <button onClick={() => props.setIsActive(!props.isActive)} className="btns__start">
-          {props.isActive ? 'Pause' : 'Start'}
-        </button>
-        <button onClick={handleReset} className="btns__reset">
-          Reset
-        </button>
-      </div>
+      <p>{`${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs
+        .toString()
+        .padStart(2, '0')}`}</p>
     </div>
   );
 };
 
 export default Timer;
-
-// import React from 'react'
-
-// const CountDownTimer = ({hoursMinSecs}) => {
-
-//     const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
-//     const [[hrs, mins, secs], setTime] = React.useState([hours, minutes, seconds]);
-
-//     const tick = () => {
-
-//         if (hrs === 0 && mins === 0 && secs === 0)
-//             reset()
-//         else if (mins === 0 && secs === 0) {
-//             setTime([hrs - 1, 59, 59]);
-//         } else if (secs === 0) {
-//             setTime([hrs, mins - 1, 59]);
-//         } else {
-//             setTime([hrs, mins, secs - 1]);
-//         }
-//     };
-
-//     const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
-
-//     React.useEffect(() => {
-//         const timerId = setInterval(() => tick(), 1000);
-//         return () => clearInterval(timerId);
-//     });
-
-//     return (
-//         <div>
-//             <p>{`${hrs.toString().padStart(2, '0')}:${mins
-//             .toString()
-//             .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p>
-//         </div>
-//     );
-// }
-
-// export default CountDownTimer;
