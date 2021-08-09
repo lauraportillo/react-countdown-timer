7abdf60edd6fe04ef23928e361f16a1541697868
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-09 23:58:53 +0200

    try find stop solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,56 @@
-// React
-import React, { useState, useEffect } from 'react';
-// Components
+// // React
+// import React, { useState, useEffect } from 'react';
+// // Components
+// import Timer from './Timer';
+// // Styles
+// import '../stylesheets/App.scss';
+
+// const App = () => {
+//   //state
+//   const [hours] = useState('');
+//   const [minutes] = useState('1');
+//   const [seconds] = useState('1');
+//   const [[hrs, mins, secs], setTime] = useState([hours, minutes, seconds]);
+
+//   const countDown = () => {
+//     if (hrs === 0 && mins === 0 && secs === 0) reset();
+//     else if (mins === 0 && secs === 0) {
+//       setTime([hrs - 1, 59, 59]);
+//     } else if (secs === 0) {
+//       setTime([hrs, mins - 1, 59]);
+//     } else {
+//       setTime([hrs, mins, secs - 1]);
+//     }
+//   };
+
+//   const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+//   useEffect(() => {
+//     const timerId = setInterval(() => countDown(), 1000);
+//     return () => clearInterval(timerId);
+//   });
+
+//   return (
+//     <div className="App">
+//       <Timer hrs={hrs} mins={mins} secs={secs} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from 'react';
 import Timer from './Timer';
-// Styles
-import '../stylesheets/App.scss';
 
-const App = () => {
-  //state
-  const [hours] = useState('');
-  const [minutes] = useState('1');
-  const [seconds] = useState('1');
-  const [[hrs, mins, secs], setTime] = useState([hours, minutes, seconds]);
-
-  const countDown = () => {
-    if (hrs === 0 && mins === 0 && secs === 0) reset();
-    else if (mins === 0 && secs === 0) {
-      setTime([hrs - 1, 59, 59]);
-    } else if (secs === 0) {
-      setTime([hrs, mins - 1, 59]);
-    } else {
-      setTime([hrs, mins, secs - 1]);
-    }
-  };
-
-  const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
-
-  useEffect(() => {
-    const timerId = setInterval(() => countDown(), 1000);
-    return () => clearInterval(timerId);
-  });
-
+function App() {
   return (
     <div className="App">
-      <Timer hrs={hrs} mins={mins} secs={secs} />
+      <header>
+        <h1> Pomodoro</h1>
+      </header>
+      <Timer />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,21 +1,65 @@
-// Styles
-import '../stylesheets/Timer.scss';
-import React from 'react';
+// // Styles
+// import '../stylesheets/Timer.scss';
+// import React from 'react';
 
-const Timer = (props) => {
+// const Timer = (props) => {
+//   return (
+//     <div>
+//       <div>
+//         <div className="counter">
+//           <span className="counter__hr"> {props.hrs.toString().padStart(2, '0')} </span>
+//           <span>:</span>
+//           <span className="counter__min">{props.mins.toString().padStart(2, '0')}</span>
+//           <span>:</span>
+//           <span className="counter__sec">{props.secs.toString().padStart(2, '0')}</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Timer;
+
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(1);
+  const [displayMessage, setDisplayMessage] = useState(false);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      clearInterval(interval);
+
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          let minutes = displayMessage ? 24 : 4;
+          let seconds = 59;
+
+          setSeconds(seconds);
+          setMinutes(minutes);
+          setDisplayMessage(!displayMessage);
+        }
+      } else {
+        setSeconds(seconds - 1);
+      }
+    }, 1000);
+  }, [seconds, minutes, displayMessage]);
+
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
   return (
-    <div>
-      <div>
-        <div className="counter">
-          <span className="counter__hr"> {props.hrs.toString().padStart(2, '0')} </span>
-          <span>:</span>
-          <span className="counter__min">{props.mins.toString().padStart(2, '0')}</span>
-          <span>:</span>
-          <span className="counter__sec">{props.secs.toString().padStart(2, '0')}</span>
-        </div>
+    <div className="pomodoro">
+      <div className="message">{displayMessage && <span> Break time!</span>}</div>
+      <div className="timer">
+        {timerMinutes}:{timerSeconds}
       </div>
     </div>
   );
-};
+}
 
 export default Timer;
