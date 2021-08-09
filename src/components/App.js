e1b8a0c3e19f5d4ffb4feb35260525dec3406071
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-09 12:21:27 +0200

    move logis to app compo

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,30 @@ import '../stylesheets/App.scss';
 
 const App = () => {
   const hoursMinSecs = { hours: 1, minutes: 20, seconds: 40 };
+  const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
+  const [[hrs, mins, secs], setTime] = React.useState([hours, minutes, seconds]);
+
+  const tick = () => {
+    if (hrs === 0 && mins === 0 && secs === 0) reset();
+    else if (mins === 0 && secs === 0) {
+      setTime([hrs - 1, 59, 59]);
+    } else if (secs === 0) {
+      setTime([hrs, mins - 1, 59]);
+    } else {
+      setTime([hrs, mins, secs - 1]);
+    }
+  };
+
+  const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+  React.useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
 
   return (
     <div className="App">
-      <Timer hoursMinSecs={hoursMinSecs} />
+      <Timer hrs={hrs} mins={mins} secs={secs} />
     </div>
   );
 };
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,31 +4,10 @@
 import '../stylesheets/Timer.scss';
 import React from 'react';
 
-const Timer = ({ hoursMinSecs }) => {
-  const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
-  const [[hrs, mins, secs], setTime] = React.useState([hours, minutes, seconds]);
-
-  const tick = () => {
-    if (hrs === 0 && mins === 0 && secs === 0) reset();
-    else if (mins === 0 && secs === 0) {
-      setTime([hrs - 1, 59, 59]);
-    } else if (secs === 0) {
-      setTime([hrs, mins - 1, 59]);
-    } else {
-      setTime([hrs, mins, secs - 1]);
-    }
-  };
-
-  const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
-
-  React.useEffect(() => {
-    const timerId = setInterval(() => tick(), 1000);
-    return () => clearInterval(timerId);
-  });
-
+const Timer = (props) => {
   return (
     <div>
-      <p>{`${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs
+      <p>{`${props.hrs.toString().padStart(2, '0')}:${props.mins.toString().padStart(2, '0')}:${props.secs
         .toString()
         .padStart(2, '0')}`}</p>
     </div>
