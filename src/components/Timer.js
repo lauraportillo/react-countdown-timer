c4a35f9fb0d1796ef8cecea2e4d3a8ac6dc8413f
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-10 12:20:17 +0200

    add monospace typo to timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,12 +3,15 @@ import '../stylesheets/Timer.scss';
 
 const Timer = (props) => {
   return (
-    <div className="timer">
-      <div className="timer__num">
-        {props.hours.toString().padStart(2, '0')}:{props.minutes.toString().padStart(2, '0')}:
-        {props.seconds.toString().padStart(2, '0')}
+    <>
+      <h1>countdown timer </h1>
+      <div className="timer">
+        <div className="timer__num">
+          {props.hours.toString().padStart(2, '0')}:{props.minutes.toString().padStart(2, '0')}:
+          {props.seconds.toString().padStart(2, '0')}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
