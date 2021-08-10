803002a36adfc2bfa1f7f3a7632e30dddabbd33e
Laura Portillo <lauraportillo.ark@gmail.com>
2021-08-10 12:55:38 +0200

    adjust styles

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,8 +4,8 @@ import '../stylesheets/Timer.scss';
 const Timer = (props) => {
   return (
     <div className="container">
-      <h1 className="container__title">countdown timer </h1>
-      <h3 className="container__subitle">time remaining</h3>
+      <h1 className="container__title">Countdown timer </h1>
+      <h3 className="container__subitle">Time remaining</h3>
       <div className="container__timer">
         <div className="container__timer--num">
           {props.hours.toString().padStart(2, '0')}:{props.minutes.toString().padStart(2, '0')}:
